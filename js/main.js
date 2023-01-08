14b36081f116234edcef70e5326c2af7ca4c2513
Muhammad Ahmad <mahmadmaqbool787@gmail.com>
2023-01-08 23:03:56 +0500

    printing the output

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 // ? Grabbing the Elements
-
+// Input Element
 const form = document.getElementById('input_form');
 const f_name = document.getElementById('f_name');
 const l_name = document.getElementById('l_name');
+const i_class = document.getElementById('class');
 const city = document.getElementById('city');
 const institute = document.getElementById('institute');
 const num_subj = document.getElementById('nos');
@@ -16,9 +17,21 @@ const next = document.getElementById('next');
 const subj_box = document.getElementById('subj_box');
 const submit = document.getElementById('submit');
 
+// Output Element
+
+const firstName = document.getElementById('o_f_name');
+const lastName = document.getElementById('o_l_name');
+const Class = document.getElementById('o_class');
+const City = document.getElementById('o_city');
+const Institute = document.getElementById('o_institute');
+const Subject = document.getElementById('subject_name');
+const Serial = document.getElementById('sr_number');
+const Total = document.getElementById('subject_total');
+const Obtained = document.getElementById('subject_obt');
+
 // Students Marks Array
-const subject = [];
 const student = {};
+const subject = [];
 // * Incrementing the counter
 let i = 1;
 next.addEventListener('click', () => {
@@ -54,7 +67,39 @@ next.addEventListener('click', () => {
 });
 
 form.addEventListener('submit', (e) => {
-  e.preventDefault;
-  alert('helo');
+  e.preventDefault();
+  student.first_name = f_name.value;
+  student.last_name = l_name.value;
+  student.class = i_class.value;
+  student.city = city.value;
+  student.institute = institute.value;
+
+  // Writing value into Element
+
+  firstName.innerHTML = student.first_name;
+  lastName.innerHTML = student.last_name;
+  Class.innerHTML = student.class;
+  City.innerHTML = student.city;
+  Institute.innerHTML = student.institute;
+
+  // Creating Elements
+  for (let i = 0; i < +num_subj.value; i++) {
+    // ! Creating Serial Numbers
+    const serialNumber = document.createElement('li');
+    serialNumber.innerHTML = i + 1;
+    Serial.appendChild(serialNumber);
+
+    // ! Creating Names of Subject
+    const subjectName = document.createElement('li');
+    subjectName.innerHTML = subject[i].name;
+    Subject.appendChild(subjectName);
+    // ! Creating Total of Subject
+    const Totals = document.createElement('li');
+    Totals.innerHTML = subject[i].total;
+    Total.appendChild(Totals);
+    // ! Creating Total of Subject
+    const Obt = document.createElement('li');
+    Obt.innerHTML = subject[i].obt;
+    Obtained.appendChild(Obt);
+  }
 });
-console.log(subject);
